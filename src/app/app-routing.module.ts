5faaaf8f5b6e16359de977e43b9b4ceb70962183
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2018-12-11 09:59:54 +1000

    create mock up for slide show

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UploadComponent } from './pages/upload/upload.component';
+import { SlideShowComponent } from './pages/slide-show/slide-show.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'upload', pathMatch: 'full' },
+  { path: 'upload', component: UploadComponent },
+  { path: 'slide-show', component: SlideShowComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
